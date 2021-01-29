48fb58751d6b5dcfc588428ab1eee9f0ac4c0f32
kirin Thapar <kirinthapar@yandex.com>
2021-01-29 14:30:34 +0000

    new

--- main.js
+++ main.js
@@ -1,87 +1,67 @@
+const cardContainer = document.querySelectorAll(".card");
+const front = document.querySelectorAll("front");
+let firstCard, secondCard;
 
-const cardContainer = document.querySelectorAll(".card")
-const front = document.querySelectorAll("front")
-let firstCard,  secondCard;
-
-const display = document.getElementById('timer');
+const display = document.getElementById("timer");
 const alertText = document.getElementById("alert-text");
 
+cardContainer.forEach((container) =>
+  container.addEventListener("click", (event) => {
+    const [front, back] = container.children;
+    front.classList.toggle("front-show");
+    back.classList.toggle("back-hide");
 
-cardContainer.forEach(container => container.addEventListener("click", (event) => { 
-    const [front, back] = container.children
-    front.classList.toggle("front-show")
-    back.classList.toggle("back-hide")
-
-
-    console.log(event);
-    console.log(container.dataset);
-    
-    
-    
-
-    
-
-    if ( firstCard === undefined) {
-        second = 0;
-        minute = 0; 
-        hour = 0;
-        startTimer();
-        return   firstCard = container.dataset.number;
-    } 
-    if ( secondCard === undefined) {
-        secondCard = container.dataset.number;
+    if (firstCard === undefined) {
+      return (firstCard = container.dataset.number);
+    }
+    if (secondCard === undefined) {
+      secondCard = container.dataset.number;
     }
 
-    if (firstCard === secondCard ) {
-        alertText.innerHTML = "<p>You found a match!</p>";
-        
-    }else{        
-        alertText.innerHTML = "<P> Keep Trying </P>";
-
-            setTimeout(() => {
-                
-                 back.classList.toggle("back-hide")
-                 
-                },2000);
-        }
-    }));
-    
-    
-    var second = 0, minute = 0;
-    var timer = document.querySelector(".timer");
-    var interval;
-    function startTimer(){
-        interval = setInterval(function(){
-            timer.innerHTML = minute+"mins "+second+"secs";
-            second++;
-            if(second == 60){
-                minute++;
-                second = 0;
-            }
-            if(minute == 60){
-                hour++;
-                minute = 0;
-            }
-        },1000);
+    if (firstCard === secondCard) {
+      alertText.innerHTML = "<p>You found a match!</p>";
+      
+    } else {
+      alertText.innerHTML = "<P> Keep Trying </P>";
+     setTimeout(() => {
+      back.classList.toggle("back-hide");
+      }, 2000);
     }
-    function moveCounter(){
-        moves++;
-        counter.innerHTML = moves;
-        //start timer on first move
-        if(moves == 1){
-            second = 0;
-            minute = 0; 
-            hour = 0;
-            startTimer();
-        }
+  })
+);
 
-
+var second = 0,
+  minute = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer() {
+  interval = setInterval(function () {
+    timer.innerHTML = minute + "mins " + second + "secs";
+    second++;
+    if (second == 60) {
+      minute++;
+      second = 0;
+    }
+    if (minute == 60) {
+      hour++;
+      minute = 0;
+    }
+  }, 1000);
+}
+function moveCounter() {
+  moves++;
+  counter.innerHTML = moves;
+  //start timer on first move
+  if (moves == 1) {
+    second = 0;
+    minute = 0;
+    hour = 0;
+    startTimer();
+  }
+}
 (function shuffle() {
-        cardContainer.forEach(container => {
-            let ramdomPos = Math.floor(Math.random() * 12);
-            container.style.order = ramdomPos;
-        });
-        })(); 
-
-
-    }
+  cardContainer.forEach((card) => {
+    let ramdomPos = Math.floor(Math.random() * 12);
+    card.style.order = ramdomPos;
+  });
+})();
